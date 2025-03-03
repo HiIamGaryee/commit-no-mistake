679c207f3634fead04fe38caa253516a626a1a03
HiIamGaryee <tgyandtpy@gmail.com>
2025-03-03 23:40:51 +0800

    add home and install what needed and setup

---
+++ src/page/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-semibold mb-2">Home Page</h2>
+    </div>
+  );
+};
+
+export default HomePage;
