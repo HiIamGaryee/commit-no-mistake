baf30f3032bd5137a7bd8ea5372685050170abbf
HiIamGaryee <tgyandtpy@gmail.com>
2025-03-04 00:02:37 +0800

    udpate ui / css/ package

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -4,6 +4,9 @@ const HomePage: React.FC = () => {
   return (
     <div className="p-6">
       <h2 className="text-2xl font-semibold mb-2">Home Page</h2>
+      <button className="neon-button bg-purple-600 hover:bg-purple-700 px-4 py-1 rounded">
+        Open AiBot
+      </button>
     </div>
   );
 };
